4e2d186862d7ddf44520916c9c3d7df178a1a0c3
Wm. Barrett Simms <wbsimms@gmail.com>
2014-09-19 14:35:59 -0400

    Initial Revision

---
+++ src/LogicEngine.Lib.Test/EngineTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicEngine.Lib.Test
+{
+    [TestClass]
+    public class EngineTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Engine engine = new Engine();
+            Assert.IsNotNull(engine);
+        }
+    }
+}
