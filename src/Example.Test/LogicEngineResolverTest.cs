46d4601e660a65cdebfe6a61fba922f01b7a31a5
Wm. Barrett Simms <barrett@wbsimms.com>
2016-06-24 15:38:54 -0500

    Remove lib dependancy on Unity

--- src/Example.Test/LogicEngineResolverTest.cs
+++ src/Example.Test/LogicEngineResolverTest.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace LogicEngine.Lib.Test
+namespace Example.Test
 {
     [TestClass]
     public class LogicEngineResolverTest
--- src/Example/LogicEngineResolver.cs
+++ src/Example/LogicEngineResolver.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Practices.Unity;
+﻿using Microsoft.Practices.Unity;
 
-namespace LogicEngine.Lib
+namespace Example
 {
     public class LogicEngineResolver
     {
